b70eb8b2bf35acdcd7322f9b71fa3d9b26499293
Assis Paiva Neto <assispaivaneto@gmail.com>
2025-07-30 15:34:06 -0300

    Adiciona tradutor MengãoLang com delay de 1s e exemplos

--- examples/campeao-liberta.py
+++ examples/campeao-liberta.py
@@ -1,12 +1,12 @@
 def main():
-    print("Digite o número de vezes que o Falmengo foi campeão da Libertadores:")
+    print("Digite o número de vezes que o Flamengo foi campeao da Libertadores: ")
     campeao = int(input())
 
     if campeao == 3:
-        print("Você acertou!! O Falmengo foi campeão da Libertadores 3 vezes")
+        print("Voce acertou!! O Falmengo foi campeão da Libertadores 3 vezes")
 
-    if campeao !=3:
-        print("Você errou!! O Falmengo não foi campeão da Libertadores {}".format(campeao))
+        if campeao != 3:
+            print("Voce errou!! O Falmengo nao foi campeão da Libertadores {}".format(campeao))
 
 if __name__ == "__main__":
     main()
--- examples/garrafas.py
+++ examples/garrafas.py
@@ -1,16 +1,12 @@
-import time
 def main():
-   garrafas = int(99)
+    garrafas = int(99)
 
-   while garrafas > 0:
-      print("{} garrafas de cerveja na parede do Maracanã".format(garrafas))
-      time.sleep(1)
-      print("{} garrafas de cerveja. Pegue uma e passe ao redor".format(garrafas))
-      time.sleep(1)
-      garrafas -= 1
-      print("{} garrafas de cerveja sobraram".format(garrafas))
-      time.sleep(1)
+    while garrafas > 0:
+        print("{} garrafas de cerveja na parede do Maracanã".format(garrafas))
+        print("{} garrafas de cerveja. Pegue uma e passe ao redor".format(garrafas))
+        garrafas -= 1
+        print("{} garrafas de cerveja sobraram".format(garrafas))
 
-   print("Não tem mais garrafas de cerveja no Maracanã")
+    print("Não tem mais garrafas de cerveja no Maracanã")
 if __name__ == "__main__":
     main()
